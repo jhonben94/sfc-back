597ec983f8085a572e724655674f758fcb19b52b
Jhony Benitez <88219328+qrsense@users.noreply.github.com>
2021-11-03 21:32:24 -0300

    Proyecto base

---
+++ src/main/java/com/py/sfc/app/SfcApplication.java
@@ -0,0 +1,13 @@
+package com.py.sfc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/py/sfc/app/SfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.py.sfc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
